e22485c709df4e185e695e6e3fa2c40ea1c59949
hoangchuong1994 <hoangvanchuong0917023924@gmail.com>
2024-12-28 10:04:15 +0700

    add new project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { fetchGender } from "@/data/fetch-gender";
+
+export default async function Home() {
+  const genders = await fetchGender();
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1>main</h1>
+        {JSON.stringify(genders, null, 4)}
+      </main>
+    </div>
+  );
+}
