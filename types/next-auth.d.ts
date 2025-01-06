dbe5d2167a928606b541f81d5fec0d96560e1517
hoangchuong1994 <hoangvanchuong0917023924@gmail.com>
2025-01-06 11:07:50 +0700

    fixed type role auth

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,18 +1,18 @@
 import { DefaultSession } from 'next-auth';
-import { UserRole, User as UserPrisma } from '@prisma/client';
+import { UserRole } from '@prisma/client';
 
 declare module 'next-auth' {
-    interface Session {
-        user: UserPrisma & DefaultSession['user'];
-    }
-
     interface User {
         role?: UserRole;
+    }
+
+    interface Session {
+        user: User & DefaultSession['user'];
     }
 }
 
 declare module 'next-auth/jwt' {
     interface JWT {
-        role?: UserRole;
+        role: UserRole;
     }
 }
